76394e0b00f872bd73a514a278066eea82a23812
wuzehv <wuzehv@qq.com>
2018-12-18 00:47:33 +0800

    add some file

---
+++ example/server_php_cgi/bc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAXLINE 1024
+
+int main() {
+    char *buf, *p;
+    char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
+    int n1, n2;
+
+    if ((buf = getenv("QUERY_STRING")) != NULL) {
+        p = strchr(buf, '&');
+        *p = '\0';
+        strcpy(arg1, buf);
+        strcpy(arg2, p+1);
+        n1 = atoi(arg1);
+        n2 = atoi(arg2);
+    }
+
+    sprintf(content, "<h1>%d + %d = %d</h1>", n1, n2, n1+n2);
+
+    printf("HTTP/1.1 200 OK\r\n");
+    printf("Content-length: %d\r\n", (int)strlen(content));
+    printf("Content-type: text/html\r\n\r\n");
+    printf("%s", content);
+    fflush(stdout);
+    exit(0);
+}
